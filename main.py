02b853ae0e4cd2a53de683ae4f5d8bcc4a4e97c7
Dawit Mellese <dawitm777@gmail.com>
2024-02-04 22:02:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+students = [ 
+
+    {'name': 'Alemayehu', 'score': 85}, 
+
+    {'name': 'Nathan', 'score': 92}, 
+
+    {'name': 'Azeb', 'score': 78}, 
+
+    {'name': 'Hailu', 'score': 95}, 
+
+    {'name': 'Gete', 'score': 88}, 
+
+    {'name': 'Abigiya', 'score': 75}, 
+
+    {'name': 'Tesfa', 'score': 90}, 
+
+    {'name': 'Desta', 'score': 82}, 
+
+    {'name': 'Tsinat', 'score': 89}, 
+
+    {'name': 'Zerihun', 'score': 93} 
+
+] 
+
+# Initialize variables to keep track of the highest score and corresponding student
+highest_score = 0
+highest_scorer = None
+
+# Iterate through the list of students
+for student in students:
+    if student['score'] > highest_score:
+        highest_score = student['score']
+        highest_scorer = student
+
+# Print the name of the highest scorer
+print(f"The student who scored the highest is: {highest_scorer['name']}")
